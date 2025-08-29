a6b1114f66935d796c995dcfb248059a2ff3135b
Grant Emeke Oseji <99779064+Grantlinkz@users.noreply.github.com>
2025-08-29 02:48:06 +0100

    Add files via upload

---
+++ Mail/sendEmail.php
@@ -0,0 +1,30 @@
+<?php
+
+        $Subject = 'Complete your Swiftcotradefx Registration';
+        $message =  "Hello ". $first_name."
+                    <br>
+                    <h1 style='color: #fff !important; background:  #FF9899; padding: 5px; text-align: center'>Swiftcotradefx</h1>
+                    <br><br>
+                    We will like to welcome you as our new client.
+                    <br>
+                    <br>
+                    For additional assistance, feel free to contact our support
+                    team via email at support@Swiftcotradefx.com
+                <br>
+                <br>
+                One more step to complete your account registration below!
+
+        <a href='https://Swiftcotradefx.com/assot/verification_code.php?verify=$complete_signup' style='color: red;'>Click here to verify your account and login </a>
+                 <br>
+                    <br>
+                 Regards,
+                 <br><br>
+                   Swiftcotradefx.
+                    <br>
+                    <br>
+                    For more information please do contact our support team on support@Swiftcotradefx.com
+                    <br>";
+        
+
+
+?>
